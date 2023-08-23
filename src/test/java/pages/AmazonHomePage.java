f40c5e5c5409791aacf9dc18c33945cdc16a5126
BijanZarean <bijanzarean@gmail.com>
2023-08-23 16:21:35 -0400

    made some changes to driver class, added notes

--- src/test/java/pages/AmazonHomePage.java
+++ src/test/java/pages/AmazonHomePage.java
@@ -20,6 +20,9 @@ public class AmazonHomePage {
 	 * driver in the memory so we can use them.
 	 */
 	
+	// The purpose of this constructor is that whenever we create an object of this class,
+	// we are initializing the page factory 
+	// and initializing the webelements within this class with the object of the driver class.
 	public AmazonHomePage() {
 		PageFactory.initElements(Driver.getDriver(), this);
 	}
